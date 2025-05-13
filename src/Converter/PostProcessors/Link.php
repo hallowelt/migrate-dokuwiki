a81c49ab2c63791fdb27e7ee915efcccd81b204d
Daniel Vogel <daniel.vogel@hallowelt.com>
2025-05-13 13:13:48 +0200

    Fix label of internal links if link ends with |]]

--- src/Converter/PostProcessors/Link.php
+++ src/Converter/PostProcessors/Link.php
@@ -12,6 +12,42 @@ class Link implements IProcessor {
 	 * @return string
 	 */
 	public function process( string $text, string $path = '' ): string {
+		$text = $this->addLabel( $text );
+		$text = $this->replacePreserveMarks( $text );
+		return $text;
+	}
+
+	/**
+	 * @param string $text
+	 * @return string
+	 */
+	private function addLabel( string $text ): string {
+		$regEx = '/###PRESERVEINTERNALLINKOPEN###(.*?)###PRESERVEINTERNALLINKCLOSE###/';
+		$pipeMarker = '###PRESERVEINTERNALLINKPIPE###';
+		$text = preg_replace_callback( $regEx, static function ( $matches ) use ( $pipeMarker ) {
+			$text = $matches[1];
+			if ( strpos( $text, $pipeMarker ) === false ) {
+				return $matches[0];
+			}
+
+			if ( strrpos( $text, $pipeMarker ) === strlen( $text ) - strlen( $pipeMarker ) ) {
+				$target = substr( $text, 0, strpos( $text, $pipeMarker ) );
+				$replacement = '###PRESERVEINTERNALLINKOPEN###';
+				$replacement .= $text . $target;
+				$replacement .= '###PRESERVEINTERNALLINKCLOSE###';
+				return $replacement;
+			}
+
+			return $matches[0];
+		}, $text );
+		return $text;
+	}
+
+	/**
+	 * @param string $text
+	 * @return string
+	 */
+	private function replacePreserveMarks( string $text ): string {
 		$text = preg_replace(
 			[
 				'/###PRESERVEINTERNALLINKOPEN###/',
